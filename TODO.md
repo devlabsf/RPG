541290d4853d7d1b7e82768bc42fd1efa6eab077
Dev Lab <admin@devlabsf.com>
2020-11-29 15:10:52 +0000

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,17 @@
+GAME PLAY
+1. When enemy is killed...
+2. When player is killed...
+3. Moving north/south/east/west
+    - use tile-based map
+4. Design list of starting players with name, stats, items
+5. Define enemies
+6. Inventory system
+7. Shop
+8. Design data structures - player, enemies, items etc.
+9. Structure for world/rooms
+
+
+LAYOUT
+1. Top bar
+  - maybe "enemy" tile could hold currently engaged thing: NPC, enemy, item
+2. Put tile map below buttons.
---
+++ js/tile.js
@@ -0,0 +1,73 @@
+var ctx = null;
+var tileW = 25, tileH = 25;
+//var mapW = 10, mapH = 10;
+var mapW = 24, mapH = 8;
+
+var currentSecond = 0, frameCount = 0, framesLastSecond = 0;
+
+// var gameMap = [
+//   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+//   0, 1, 1, 1, 0, 1, 1, 1, 1, 0,
+//   0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
+//   0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+//   0, 1, 0, 1, 0, 0, 0, 1, 1, 0,
+//   0, 1, 0, 1, 0, 1, 0, 0, 1, 0,
+//   0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+//   0, 1, 0, 0, 0, 0, 0, 1, 0, 0,
+//   0, 1, 1, 1, 0, 1, 1, 1, 1, 0,
+//   0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+// ];
+
+var gameMap = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0,
+  0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0,
+  0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0,
+  0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+];
+
+window.onload = function()
+{
+  ctx = document.getElementById("game").getContext("2d");
+  requestAnimationFrame(drawGame)
+  ctx.font = "bold 10pt sans serif"
+}
+
+function drawGame()
+{
+  if (ctx==null) {return; }
+
+  var sec = Math.floor(Date.now()/1000);
+  if (sec!=currentSecond)
+  {
+    currentSecond = sec;
+    framesLastSecond = frameCount;
+    frameCount = 1;
+  }
+  else { frameCount++; }
+
+  for(var y = 0; y < mapH; y++)
+  {
+    for(var x = 0; x < mapW; x++)
+    {
+      switch(gameMap[((y*mapW)+x)])
+      {
+        case 0:
+          ctx.fillStyle = "#999999";
+          break;
+        default:
+          ctx.fillStyle = "#eeeeee";
+      }
+
+      ctx.fillRect(x*tileW,y*tileH,tileW,tileH);
+    }
+  }
+
+  ctx.fillStyle = "#ff0000";
+  ctx.fillText("FPS:" + framesLastSecond, 10, 20);
+
+  requestAnimationFrame(drawGame)
+}
