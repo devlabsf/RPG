8c72b89afeeb61a25ad3ead10864968f25b4f605
Dev Lab <admin@devlabsf.com>
2020-12-02 21:06:41 +0000

    Upgrading tile movement

--- js/tile.js
+++ js/tile.js
@@ -1,9 +1,9 @@
-var ctx = null;
+// make these const
+var ctx = document.getElementById("game").getContext("2d");
+ctx.font = "bold 10pt sans serif"
 var tileW = 25, tileH = 25;
 //var mapW = 10, mapH = 10;
 var mapW = 24, mapH = 8;
-
-var currentSecond = 0, frameCount = 0, framesLastSecond = 0;
 
 // var gameMap = [
 //   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
@@ -19,49 +19,35 @@ var currentSecond = 0, frameCount = 0, framesLastSecond = 0;
 // ];
 
 var gameMap = [
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0,
-  0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0,
-  0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0,
-  0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
-window.onload = function()
-{
-  ctx = document.getElementById("game").getContext("2d");
-  requestAnimationFrame(drawGame)
-  ctx.font = "bold 10pt sans serif"
-}
-
-function drawGame()
-{
-  if (ctx==null) {return; }
-
+function drawGame() {
   var sec = Math.floor(Date.now()/1000);
-  if (sec!=currentSecond)
-  {
+  if (sec!=currentSecond)  {
     currentSecond = sec;
     framesLastSecond = frameCount;
     frameCount = 1;
   }
-  else { frameCount++; }
-
-  for(var y = 0; y < mapH; y++)
-  {
-    for(var x = 0; x < mapW; x++)
-    {
-      switch(gameMap[((y*mapW)+x)])
-      {
+  else { 
+    frameCount++; 
+  }
+  for (var y = 0; y < mapH; y++) {
+    for (var x = 0; x < mapW; x++) {
+      switch(gameMap[y][x]) {
         case 0:
           ctx.fillStyle = "#999999";
           break;
         default:
           ctx.fillStyle = "#eeeeee";
       }
-
       ctx.fillRect(x*tileW,y*tileH,tileW,tileH);
     }
   }
@@ -71,3 +57,5 @@ function drawGame()
 
   requestAnimationFrame(drawGame)
 }
+
+requestAnimationFrame(drawGame)
